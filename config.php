2b0f8ccf1b0b511953e27ed1ea9f4dc918fafcef
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-03-15 10:32:10 +0400

    test commit

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ define('DB_SERVER', 'support_db');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'password');
 define('DB_NAME', 'support');
-var_dump(1);
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-var_dump(2);
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 
 // Include config file
 require_once "config.php";
-
+var_dump(1);
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
@@ -86,6 +86,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     mysqli_close($link);
 }
+var_dump(2);
 ?>
 
 <!DOCTYPE html>
