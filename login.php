dede51766e40597b2f209df7b243f1aea63d7c3b
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-03-15 11:24:09 +0400

    Login functionality

--- login.php
+++ login.php
@@ -8,80 +8,68 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     exit;
 }
 
+// DB values
+$host = 'support_db';
+$db = 'support';
+$user = 'root';
+$password = 'password';
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
 
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter username.";
-    } else{
+    } else {
         $username = trim($_POST["username"]);
     }
 
     // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter your password.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
 
     // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    if (empty($username_err) && empty($password_err)) {
+        try {
+            $pdo = new PDO($dsn, $user, $password);
+            if ($pdo) {
+                $stmt = $pdo->query("SELECT id, username, password FROM users WHERE username = " . $username);
+                $user = $stmt->fetch();
+var_dump($user);exit;
+                if ($user) {
+                    if (password_verify($password, $hashed_password)) {
+                        // Password is correct, so start a new session
+                        session_start();
 
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
+                        // Store data in session variables
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["id"] = $id;
+                        $_SESSION["username"] = $username;
 
-            // Set parameters
-            $param_username = $username;
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-                            // Redirect user to welcome page
-                            header("location: welcome.php");
-                        } else{
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
-                        }
+                        // Redirect user to welcome page
+                        header("location: welcome.php");
+                    } else {
+                        // Password is not valid, display a generic error message
+                        $login_err = "Invalid username or password.";
                     }
-                } else{
-                    // Username doesn't exist, display a generic error message
+                } else {
+                    // Password is not valid, display a generic error message
                     $login_err = "Invalid username or password.";
                 }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
             }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
+        } catch (Exception $ex) {
+            //Log Exception
+            var_dump($ex->getMessage());
         }
     }
-
-    // Close connection
-    mysqli_close($link);
 }
 ?>
 
