e4a3ab50d13a7b0e86bb99829a0be86552cd61ce
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-03-15 10:36:38 +0400

    test commit

--- config.php
+++ config.php
@@ -8,9 +8,11 @@ $password = 'password';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
 $pdo = new PDO($dsn, $user, $password);
-
+var_dump(1);
 // Check connection
 if($pdo === false){
+    var_dump(3);
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+var_dump(2);
 ?>
--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 
 // Include config file
 require_once "config.php";
-
+var_dump(4);
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
