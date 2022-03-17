d7bf31901ad6fb89ee11718cdd698cc55d0faaef
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-03-17 12:47:14 +0400

    UI fixes, added logout functionality

--- index.php
+++ index.php
@@ -70,7 +70,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </script>
 </head>
 <body>
-
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+    <h5 class="my-0 mr-md-auto font-weight-normal">2288</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="#">Features</a>
+        <a class="p-2 text-dark" href="#">Enterprise</a>
+        <a class="p-2 text-dark" href="#">Support</a>
+        <a class="p-2 text-dark" href="#">Pricing</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="logout.php">Sign out</a>
+</div>
 <div class="container">
     <div class="py-5 text-center">
         <img class="d-block mx-auto mb-4" src="images/logo.jpg" alt="" width="175">
