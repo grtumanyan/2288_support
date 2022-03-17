5da92c6a8a3aea947afe1a2025c52c5ec9390948
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-03-17 16:39:00 +0400

    Visionflow API integration

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once $_SERVER['DOCUMENT_ROOT']."/visionFlowService.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/visionFlowService.php";
 
 try {
     $result = findProjectIssues();
