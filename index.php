4cac664c80500e0f7e42429e9781e1d589dafd95
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-03-22 12:02:53 +0400

    Page UI fixes

--- index.php
+++ index.php
@@ -179,7 +179,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 
-    <footer class="footer">
+    <footer class="footer mt-auto py-3 bg-light">
         <div class="container">
             <span class="text-muted">© 2288</span>
         </div>
