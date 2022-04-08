6e4499840ef66142e66caefd1d07ec7d069241aa
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-04-08 16:29:06 +0400

    Visionflow API integration

--- visionFlowService.php
+++ visionFlowService.php
@@ -101,7 +101,7 @@ function storeProjectDocument($client, $file) {
     return $client->storeProjectDocument(array(
         'document' => array(
             'createdBySystemUserId' => 0,
-            'documentSize' => 0,
+            'documentSize' => 161616,
             'folderId' => 0,
             'inheritPermissions' => 0,
             'inheritPermissionsHasBeenSet' => 0,
