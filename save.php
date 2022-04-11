dde1a3b3cc3d4363bb8ed403432021ee363ad846
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-04-11 13:41:51 +0400

    Visionflow API integration

--- save.php
+++ save.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once $_SERVER['DOCUMENT_ROOT'] . "/visionFlowService.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/pointsCountingService.php";
 
