8a8635b26468e345f1937dba2dfa79af4177fcfb
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-04-11 14:46:19 +0400

    Visionflow API integration

--- pointsCountingService.php
+++ pointsCountingService.php
@@ -222,10 +222,8 @@ function countPoints($data) {
 
     foreach ($data as $key=>$value) {
         if (array_key_exists(str_replace('_', ' ', $key), $points[$animal])){
-            if (array_key_exists(str_replace('_', ' ', $key), $points[$animal][str_replace('_', ' ', $key)][$value])) {
-                $point = $points[$animal][str_replace('_', ' ', $key)][$value];
-                $count += $point;
-            }
+            $point = $points[$animal][str_replace('_', ' ', $key)][$value];
+            $count += $point;
         }
     }
     var_dump($count);exit;
