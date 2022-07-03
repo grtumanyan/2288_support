dc1529d6e2a957c2e600ec69f34a76afc5ba14fc
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-07-03 05:37:51 +0400

    Standard questions implemented

--- standard.php
+++ standard.php
@@ -14,37 +14,18 @@ $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = array_map ( 'htmlspecialchars' , $_POST );
-
-    var_dump($_POST);exit;
+    $new_standard = $_POST['standard'];
 
     try {
         $pdo = new PDO($dsn, $user, $password);
 
-        $dataForDB = json_encode($data);
+        $dataForDB = json_encode($new_standard);
         if ($pdo) {
-            $query = "INSERT INTO answers (data) VALUES('$dataForDB')";
+            $query = "INSERT INTO standard (data) VALUES('$dataForDB')";
             $pdo->exec($query);
-            $fileUniqueName = uniqid().".txt";
-            $fileName = $_SERVER['DOCUMENT_ROOT'] . "/docs/".$fileUniqueName;
-
-            $points = $_SESSION["ticket_points"];
-            if ($points < 50) {
-                $data['beslutsstödets_svar'] = 'Går bra att avvakta hemma, Rådgivning från 2288';
-            } elseif ($points >= 50 && $points <= 200) {
-                $data['beslutsstödets_svar'] = 'Kontakta Firstvet';
-            } elseif ($points > 200) {
-                $data['beslutsstödets_svar'] = 'Åk till klinik';
-            }
-
-            $file = file_put_contents($fileName, print_r($data, true));
-            $ticket = $_SESSION["ticket"];
-            $ticketPrimaryKey = $_SESSION["ticket_primaryKey"];
-
-            $client = login();
-            $result = (array)storeIssueDocument($client, $fileName, $ticketPrimaryKey, $fileUniqueName);
-            $_SESSION["ticket_points"] = $points;
-            unlink($fileName);
         }
+        header('Location: index.php');
+        exit;
     }catch(Exception $ex){
         //Log Exception
         var_dump($ex->getMessage());
