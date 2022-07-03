53fd6345ec2192dcfefb48294fbcad0e0c050416
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-07-03 05:47:10 +0400

    Standard questions implemented

--- index.php
+++ index.php
@@ -147,29 +147,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#" style="margin-left: 10px;"><?= $_SESSION["username"]; ?></a>
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown05">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>
-    </ul>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="standard.php" style="margin-left: 10px;">Standard questions</a>
+
     <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <a class="nav-link" href="standard.php">Standard questions</a>
-        </li>
         <li class="nav-item">
             <a class="nav-link" href="logout.php">Sign out</a>
         </li>
