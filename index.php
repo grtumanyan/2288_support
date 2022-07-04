ea9399012b0bed5e4ee3e2c0efa99e1149c3feed
Grigor Tumanyan <grigor.tumanyan@devolon.fi>
2022-07-04 10:19:46 +0400

    Updated bootstrap version

--- index.php
+++ index.php
@@ -85,16 +85,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title></title>
-    <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-          integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-          crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script
             src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
             crossorigin="anonymous"></script>
-
     <script type="text/javascript">
         // var url = '/apiv1.php';
         var url = '/apiv2.php';
