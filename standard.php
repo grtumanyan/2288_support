8e26242d25a24e878ef8bf0568a80ec682b761f6
grigortumanyan <grtumanyan@gmail.com>
2022-12-14 11:09:01 +0400

    Made standard page mandatory authenticated

--- standard.php
+++ standard.php
@@ -5,6 +5,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Check if the user is logged in, otherwise redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
 $host = 'localhost';
 $db = 'support';
 $user = 'support';
